145ea1a80c5f71bac82129d5d0c9554e33e0bdc4
Pulasthi Bethmage <87483405+pulabethmage@users.noreply.github.com>
2021-07-24 18:30:06 +0530

    First Commit

---
+++ src/main/java/DisplayServlet.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/DisplayServlet")
+public class DisplayServlet extends HttpServlet{
+
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String name = "Pulasthi";
+		
+		req.setAttribute("myname", name);
+		RequestDispatcher rd = req.getRequestDispatcher("index.jsp");
+		rd.forward(req, resp);
+	}
+	
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+    
+<%-- <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>  --%>
+     
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+
+ <%
+ 
+		String myname = request.getAttribute("myname").toString();
+ 		out.println("My Name is : "+ myname);
+ 
+ %>
+
+<!-- <c:out value="Hello World!" /> -->
+
+</body>
+</html>
